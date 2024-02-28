bc6583c0942f6b515745b082e6319557dc172d35
Elaidzha1940 <eli080293@gmail.com>
2024-02-28 19:31:02 +0300

    InstagramUIClone

---
+++ InstagramUIClone/Feed/FeedViewController.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: InstagramUIClone
+//  File: FeedViewController.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 28.02.2024
+//
+//  */
+
+import UIKit
+import SnapKit
+
+class FeedViewController: UIViewController {
+    //MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
