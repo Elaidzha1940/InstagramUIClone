ba0bd5a0c44be2e67a07028f51da6267070fbf52
Elaidzha1940 <eli080293@gmail.com>
2024-02-29 15:10:41 +0300

    Added a LogoView

---
+++ InstagramUIClone/Views/LogoView.swift
@@ -0,0 +1,50 @@
+//  /*
+//
+//  Project: InstagramUIClone
+//  File: LogoView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 29.02.2024
+//
+//  */
+
+import UIKit
+import SnapKit
+
+class LogoView: UIView {
+    //MARK: Init
+    init() {
+        super.init(frame: .zero)
+        initialize()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: Private constants
+    
+    private enum UIConstants {
+        static let LogoWidth: CGFloat = 104
+        static let LogoHeight: CGFloat = 30
+    }
+    
+    //MARK: Private properties
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "insta")
+        return view
+    }()
+}
+
+//MARK: Private methods
+
+private extension LogoView {
+    func initialize() {
+        addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalTo(UIConstants.LogoWidth)
+            make.height.equalTo(UIConstants.LogoHeight)
+        }
+    }
+}
