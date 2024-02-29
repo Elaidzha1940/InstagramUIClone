aa48a28007e8197a4edeac0fcdfcb6e65c7164f0
Elaidzha1940 <eli080293@gmail.com>
2024-02-29 14:40:57 +0300

    Update FeedViewController / Added makeLeftBarButtonItems

--- InstagramUIClone/Feed/FeedViewController.swift
+++ InstagramUIClone/Feed/FeedViewController.swift
@@ -21,6 +21,23 @@ class FeedViewController: UIViewController {
 //MARK: Private methods
 private extension FeedViewController {
     func initialize() {
+        view.backgroundColor = .gray
+        navigationItem.leftBarButtonItems = makeLeftBarButtonItems()
+    }
+    
+    func makeLeftBarButtonItems() -> [UIBarButtonItem] {
+        let dropDownButtonItem = UIBarButtonItem(title: nil, image: UIImage(systemName: "chevron.down"), target: self, action: nil, menu: makedropDownMenu())
+        return [dropDownButtonItem]
+    }
+    
+    func makedropDownMenu() -> UIMenu {
+        let subsItem = UIAction(title: "Subscriptions", image: UIImage(systemName: "person.2.fill")) { _ in
+            print("Subs")
+        }
         
+        let favsItem = UIAction(title: "Favorites", image: UIImage(systemName: "star.fill")) { _ in
+            print("Favs")
+        }
+        return UIMenu(title: "", children: [subsItem, favsItem])
     }
 }
